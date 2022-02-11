3d77f2cc3fe0a1abafe0d3a864500a8b7e3917d1
limburamu <limburamu23@gmail.com>
2022-02-11 10:22:45 +0545

    git enable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import{UserComponent} from './user/user.component'
+import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path:'user/:id',
+    component:UserComponent
+  },
+  {
+    path:'admin',
+    component:AdminComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'',
+    component:HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
